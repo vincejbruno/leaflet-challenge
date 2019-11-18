4c736bc54f9a226a2adac1cc77880118603e7d34
Vince <Vince@MacBook-Air.local>
2019-11-17 21:13:01 -0600

    second commit

--- Mapping-HW/Homework/static/js/logic.js
+++ Mapping-HW/Homework/static/js/logic.js
@@ -64,23 +64,9 @@ function createMap(quakes) {
       collapsed: false
     }).addTo(myMap);
 
-  // Legend
-//   var legend = L.control({position: 'bottomright'});
 
-//     legend.onAdd = function(myMap){
-//       var div = L.DomUtil.create('div', 'info legend'),
-//         grades = [4, 5],
-//         labels = [];
-
-    // for (var i = 0; i < grades.length; i++) {
-    //     div.innerHTML +=
-    //         '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-    //         grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    // }
 return div;
-    // };
 
-    // legend.addTo(myMap);
 }
    
 
